29b43d18285322f750587ab057258302192cc55e
krishivvyas <krishivvyas1234@gmail.com>
2026-02-07 18:18:51 +0530

    ui changes small

--- app/page.tsx
+++ app/page.tsx
@@ -45,24 +45,24 @@ export default function Home() {
             transition={{ delay: 0.5 }}
             className="text-primary mb-2 md:mb-4"
           >
-            <span className="text-4xl md:text-6xl font-bold">_&gt;</span>
+            <span className="text-5xl md:text-7xl font-bold">&gt;_</span>
           </motion.div>
 
-          <h1 className="text-6xl md:text-9xl font-black tracking-tighter text-[#39FF14] drop-shadow-[0_0_15px_rgba(57,255,20,0.5)]">
+          <h1 className="text-4xl md:text-7xl pr-6 font-black tracking-tighter text-[#39FF14] drop-shadow-[0_0_15px_rgba(57,255,20,0.5)]">
             DEVSTORY
           </h1>
         </div>
 
         {/* Subtitle & Tagline */}
         <div className="space-y-4">
-          <p className="text-xl md:text-2xl text-muted-foreground/80 tracking-wide">
+          <p className="text-l md:text-xl text-muted-foreground/80 tracking-wide">
             Every line of code has it's own story
           </p>
           <motion.p
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1, duration: 1 }}
-            className="text-xl md:text-2xl text-primary font-bold tracking-wider"
+            className="text-l md:text-xl text-primary font-bold tracking-wider"
           >
             {">"} Generate winning packages instantly.
           </motion.p>
